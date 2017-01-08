f51b75ce6a150fc85842e2808ac6b4ab85ebfae7
jacksparrow00 <mehndiratta.nitish038@gmail.com>
2017-01-08 16:47:22 +0530

    1st version

---
+++ On the map/InformationPostingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  InformationPostingViewController.swift
+//  On the map
+//
+//  Created by Nitish on 06/01/17.
+//  Copyright © 2017 Nitish. All rights reserved.
+//
+
+import UIKit
+
+class InformationPostingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
